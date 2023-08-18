a60d598f654065a0ed502ace841425c3c1ddd964
bnature <102365880+bnature@users.noreply.github.com>
2023-08-18 15:03:07 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+radio.setGroup(11)
+radio.setTransmitPower(7)
